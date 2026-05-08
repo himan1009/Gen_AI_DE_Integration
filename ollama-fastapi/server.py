643ca94b73a12cbd1362d48fd1b3d396533b9986
himan1009 <shishusha922@gmail.com>
2026-05-08 12:22:15 +0530

    ollama things

---
+++ ollama-fastapi/server.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Body
+from ollama import Client
+
+app = FastAPI()
+client = Client(
+    host="http://localhost:11434",
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/contact-us")
+def read_root():
+    return {"email": "shishusha922@gmail.com", "phone": "+91-1234567890"}
+
+
+@app.post("/chat")
+def chat(
+        message: str = Body(..., description="The message to send to the model")
+):
+    response = client.chat(
+        model="gemma:2b",
+        messages=[{"role": "user", "content": message}]
+    )  
+
+    return {"response": response.message.content}
